d2e190bdf5f4e871a1ce87b69c16c776cac176bf
manan2110 <manan.co.in@gmail.com>
2021-09-05 18:03:33 +0530

    Small fixed

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -3,10 +3,18 @@ import CardTiles from "../Components/CardTiles";
 import "../CSS/Home.css";
 
 export default function Home({ pharmacies }) {
-	return (
-		<div className="home-container">
-			{pharmacies &&
-				pharmacies.map((pharmacy) => <CardTiles pharmacy={pharmacy} />)}
-		</div>
-	);
+	const user = localStorage.getItem('token');
+	if (user)
+		return (
+			<div className="home-container">
+				{pharmacies &&
+					pharmacies.map((pharmacy) => <CardTiles pharmacy={pharmacy} />)}
+			</div>
+		);
+	else
+		return (
+			<div className="login-error">
+				Login to continue.
+			</div>
+		)
 }
